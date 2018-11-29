81a2dc8722c6e58a88830d29aef0daff3527ecd5
Rushabh Nisher <nisher.r@husky.neu.edu>
2018-11-29 17:54:39 -0500

    Added Terms and conditions component

--- ZapChat/src/app/app.module.ts
+++ ZapChat/src/app/app.module.ts
@@ -7,17 +7,23 @@ import { AppComponent } from './app.component';
 
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { TermsComponent } from './terms/terms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignUpComponent
+    SignUpComponent,
+    TermsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
+      {
+        path: 'terms',
+        component: TermsComponent
+      },
       {
         path: 'signUp',
         component: SignUpComponent
