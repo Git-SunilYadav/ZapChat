be0c3a94ee5b1bf89ab9f9c0a6ba1d21fcd77f9b
Sunil Yadav <yadav.su@husky.neu.edu>
2018-12-06 18:15:15 -0500

    Integrated contact list with firebase

--- ZapChat/src/app/contact-list/contact-list.component.ts
+++ ZapChat/src/app/contact-list/contact-list.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireDatabase } from 'angularfire2/database'
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.scss']
 })
-export class ContactListComponent implements OnInit {
+export class ContactListComponent  {
 
-  constructor() { }
+  contactList: any[];
+  constructor(db: AngularFireDatabase) {
+    db.list('/chat/8237292660/contactList/')
+    .valueChanges().subscribe(contact => {
+      this.contactList = contact;
+      console.log(this.contactList);
+
+    });
+   }
 
   ngOnInit() {
+  
   }
 
 }
