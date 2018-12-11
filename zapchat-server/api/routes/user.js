799f51cdc8dea92fae770be89cf215371bb318de
panchalkiran <panchal.k@northeastern.edu>
2018-12-11 13:08:57 -0500

    create message api

--- zapchat-server/api/routes/user.js
+++ zapchat-server/api/routes/user.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-var user = require('../models/user.js');
-
 const APP = require('../../firebase_configuration.js');
 const db = APP.registerApplication().database();
 
+//api to show all user details
 router.get('/', (req, res, next) => {
     res.status(200).json({
         message: 'user details fetched'
@@ -50,6 +49,7 @@ router.post('/', (req, res, next) => {
 });
 
 
+//api for login
 router.post('/login/', (req, res, next) => {
     var ref = db.ref("chat/");
     AuthenticateUser(ref, req, res, next,  function (data) {
@@ -72,6 +72,63 @@ router.post('/login/', (req, res, next) => {
 
     });
 });
+
+
+//api to send message
+router.post('/sendMessage/', (req, res, next) => {
+    let ref = db.ref("chat/");
+    SendMessage(ref, req.body.senderNumber,req.body.receiverNumber,req.body.message, res, next, 'sent', function (data) {
+        let newReceiver = req.body.senderNumber;
+        let newSender = req.body.receiverNumber;
+        let message = req.body.message
+        req.body.receiverNumber = newReceiver;
+        req.body.senderNumber = newSender;
+        
+        SendMessage(ref,newSender, newReceiver, message, res, next, 'received',  function (data) {
+            if(data){
+                res.status(200).json({
+                    message: message,
+                    senderNumber: req.body.newSender,
+                    receiverNumber: req.body.newReceiver
+                }).end();
+                
+            }
+            else
+            {
+                res.status(500).json({
+                    message: 'Message not sent'
+                }).end();
+            }
+        });
+
+    });
+});
+
+// Function to send message 
+function SendMessage(ref, senderNumber,receiverNumber,message , res, next, messageType,callback) {
+    ref.once("value", function (snap) {
+        snap.forEach(function (data) {
+            if (data.key == senderNumber) {
+                let newRef = db.ref("chat/" + senderNumber + "/contactList/");
+                newRef.once("value", function (snap) {
+                    snap.forEach(function (data){
+                        if (data.key == receiverNumber){
+                            let msgRef = db.ref("chat/" + senderNumber + "/contactList/"+ receiverNumber + "/chats/");
+                            msgRef.push().set({
+                                message: message,
+                                type: messageType
+                            });
+                        }
+                    });
+                });
+                //callback(true);
+            }
+        });
+        //callback(false);
+    });
+    callback(true);
+};
+
 
 // Function to validate user 
 function AuthenticateUser(ref, req, res, next,callback) {
